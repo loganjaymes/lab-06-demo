bbcaf39407157628ce82d9f817a2ffd669f92cde
Logan Bjork <loganbjork@ufl.edu>
2023-10-25 14:03:36 -0400

    added comments in encode for better clarification

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ def encode(password):
 
     for x in str(password):
         newNumToAdd = (int(x) + 3)
+        # encodes the password by adding 3 to each number inside the password string
         if newNumToAdd >= 10:
             newNumToAdd = newNumToAdd % 10
-            # removes the tens place for the new number, so it is not over two digits
+            # removes the tens place for the new number, so it's not over two digits
             newPassword += str(newNumToAdd) 
         else:
             newPassword += str(newNumToAdd)
